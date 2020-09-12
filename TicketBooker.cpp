76b15e437e888addee6ab0e7a74b17b072a50a88
HiImTabi <bobtabiin@gmail.com>
2020-09-12 22:18:21 +0930

    trying to integrate queue 2, trying to use priority queues, probably not gonna work out

--- TicketBooker.cpp
+++ TicketBooker.cpp
@@ -29,8 +29,8 @@ class process
 		int running = 0;
 		int waiting = 0;
 
-		void print_process();
-		void print_details();
+		void print_process() const;
+		void print_details() const;
 };
 
 process::process(std::string new_id, int new_arrival, int new_priority, int new_age, int new_tickets)
@@ -42,7 +42,7 @@ process::process(std::string new_id, int new_arrival, int new_priority, int new_
     tickets = new_tickets;
 }
 
-void process::print_process()
+void process::print_process() const
 {
 	cout << id;
 	cout << " " << arrival;
@@ -51,7 +51,7 @@ void process::print_process()
 	cout << " " << tickets << endl;
 }
 
-void process::print_details()
+void process::print_details() const
 {
     cout << id;
     cout << " " << arrival;
@@ -78,7 +78,7 @@ struct queue1
 };
 
 void print_subqueue(std::queue<process>);
-bool process_customer(process&, int&);
+bool process_customer_q1(process&, int&);
 
 // Prints all the processes in a queue
 void print_subqueue(queue<process> subqueue)
@@ -128,16 +128,68 @@ bool queue1::is_empty()
     return false;
 }
 
+// Struct queue2: this struct represents queue 2
+// Queue 2 is represented by a priority queue i.e. a min heap.
+// This min heap of customers is sorted according to number of remaining tickets.
+
+// Overload operator for priority queue to work with our process class
+bool operator<(const process& a, const process& b) 
+{
+	return a.tickets > b.tickets;
+}
+
+struct queue2
+{
+    priority_queue<process> que2;
+
+    void print_queue2();
+    void add_to_queue2(process new_process);
+    bool is_empty();
+};
+
+void print_subqueue(std::queue<process>);
+bool process_customer_q1(process&, int&);
+
+// Prints all the processes in queue 1 ONLY
+// Printed in order of priority.
+void queue2::print_queue2()
+{
+	cout << "Printing Queue2" << endl;
+	while (!que2.empty()) {
+		que2.top().print_process();
+		que2.pop();
+	}
+
+	cout << endl;
+}
+
+// Adds a process of priority 1 - 3 to queue1.
+// It should be impossible to add a process with any other priority.
+void queue2::add_to_queue2(process new_process)
+{
+    if (new_process.priority > 3) {
+        que2.push(new_process);
+    } else {
+        cout << "***Q2 - SOMETHING IS WRONG: Added invalid process***" << endl;
+        cout << "Caused by priority " << new_process.priority << endl; 
+    }
+}
+
+bool queue2::is_empty()
+{
+	return que2.empty();
+}
+
 static int current_time = 0;
 static queue<process> input;
 static queue1 q1;
+static queue2 q2;
 
 // Process a customer with queue 1 logic, i.e. Weighted Round Robin.
 // Returns true if all of the customer's tickets are processed.
 // Else, returns false.
-bool process_customer(process& customer)
+bool process_customer_q1(process& customer)
 {
-
     int weighted_time_quantum = (10 - customer.priority) * 10;
     int tickets_processed = weighted_time_quantum/time_unit;
 
@@ -159,13 +211,11 @@ bool process_customer(process& customer)
         customer.running += customer.tickets * time_unit;
         current_time += customer.tickets * time_unit;
 
-
         customer.tickets = 0;
         
         customer.end = current_time;
         customer.last_time_processed = current_time;
 
-		// cout << "finished processing" << endl;
         customer.print_details();
         return true;
     } else {
@@ -230,8 +280,9 @@ queue<process> vector_to_queue(vector<process> list)
 	return new_queue;
 }
 
-// Adds customer in input queue to queue 1 and queue 2.
 // It stops adding customers to these queues when current_time < process.arrival.
+// i.e. when next customer can't be added at this point in time.
+// Also stops when there are no more customers to add.
 void add_to_queues()
 {
 	if (input.empty()) return;
@@ -242,14 +293,12 @@ void add_to_queues()
 
 		// TESTING
 		// cout << "added at: " << current_time << endl; 
-
-		// cout << "added at " << current_time << endl;
 		// temp.print_process();
 
 		if (temp.priority < 4) {
 			q1.add_to_queue1(temp);
 		} else {
-			// add to queue2
+			q2.add_to_queue2(temp);
 		}
 
 		input.pop();
@@ -263,28 +312,25 @@ void add_to_queues()
 /* 
 The following logic applies to all subqueues of queue 1.
 If a customer has all of its tickets processed, it is removed from the queue.
-Else, it is moved to the back of queue 1. If the customer's priority number changed,
-it should be added to the appropriate subqueue.
+Else, it is moved to the back of the appropriate subqueue of queue 1.
 */
 
 void process_subqueue1() 
 {
-    if (process_customer(q1.subqueue1.front())) {
+    if (process_customer_q1(q1.subqueue1.front())) {
         q1.subqueue1.pop();
     } else {
         process temp = q1.subqueue1.front();
         q1.subqueue1.pop();
 		add_to_queues();
 
-		// cout << "after adding" << endl;
-		// q1.print_queue1();
         q1.add_to_queue1(temp);
     }
 }
 
 void process_subqueue2() 
 {
-    if (process_customer(q1.subqueue2.front())) {
+    if (process_customer_q1(q1.subqueue2.front())) {
         q1.subqueue2.pop();
     } else {
         process temp = q1.subqueue2.front();
@@ -297,7 +343,7 @@ void process_subqueue2()
 
 void process_subqueue3() 
 {
-    if (process_customer(q1.subqueue3.front())) {
+    if (process_customer_q1(q1.subqueue3.front())) {
         q1.subqueue3.pop();
     } else {
         process temp = q1.subqueue3.front();
@@ -328,7 +374,6 @@ void process_queue1()
 	}
 }
 
-// Processes all the customers in queue 1 and queue 2
 void process_queues()
 {
 	process_queue1();
@@ -342,30 +387,19 @@ void process_tickets()
 	while (!input.empty()) {
 		add_to_queues();
 
-		process_queues();
+		// process_queues();
 
-		/* 
-		What if our queues are empty, but there are still customers in the input queue?
-		There are no tickets being processed, but we will be stuck in this while loop.
-		Since there is nothing to keep time "going".
-		I'm thinking there should be a check here that increments current_time, until a new
-		process from the input queue can be added to the queues.
-		*/
-
-		if (q1.is_empty()) {
+		if (q1.is_empty() && q2.is_empty()) {
 			current_time += time_unit;
 		}
 	}
 
-	// POSSIBLY need to do a while-loop here, which keeps looping
-	// until q1 and q2 are empty.
-	// At that point, we can say all customers have been processed.
-	while (!q1.is_empty()) {
-		process_queues();
+	// while (!q1.is_empty() || !q2.is_empty()) {
+	// 	process_queues();
 
-		// cout << "after processing" << endl;
-		// q1.print_queue1();
-	}
+	// 	// cout << "after processing" << endl;
+	// 	// q1.print_queue1();
+	// }
 }
 
 int main(int argc, char *argv[])
@@ -384,16 +418,18 @@ int main(int argc, char *argv[])
 	// Sorts input by arrival
 	stable_sort(input1.begin(), input1.end(), arrival_compare);
 
-	// By end of program, input_queue should be empty (all customers added to queues).
+	// By end of program, input should be empty.
 	input = vector_to_queue(input1);
 
 	// TESTING
-	// cout << "printing queue of input" << endl;
-	// print_subqueue(input);
-	// cout << endl;
+	cout << "printing queue of input" << endl;
+	print_subqueue(input);
+	cout << endl;
 
 	cout << "name arrival end ready running waiting" << endl;
 	process_tickets();
+	q2.print_queue2();
+
 
 	return 0;
 }
